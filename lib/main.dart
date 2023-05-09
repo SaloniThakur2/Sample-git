a77bcdfd435e31e952f2d20c8415577301c8c7b0
Saloni Thakur <thakursaloni000@gmail.com>
2023-05-09 22:05:58 +0530

    this is lib

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text('Sallu Thakur',
+    textDirection: TextDirection.ltr,
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.blue,),
+    )
+  );
+}
+
